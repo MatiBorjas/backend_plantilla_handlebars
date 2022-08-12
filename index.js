1a29a2a1809e3c586e53e5c00d0d96a71394ee4c
Matias Borjas <matias.borjas@gmail.com>
2022-08-12 11:06:52 -0300

    Desafio plantilla Handlebars

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ const server = app.listen(PORT, () => {
 });
 
 server.on('error', (error) => console.log(`Error en servidor ${error}`));
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.set('view engine', 'hbs');
-app.set('views', './views');
+//Configuracion del motor
+app.set('view engine', 'hbs'); //configuro el motor hbs
+app.set('views', './views'); //donde voy a poner mis vistas, en la carpeta views
+//como dejo configurado el motor
 app.engine(
   'hbs',
   engine({
+    //objeto de configuracion del motor
     extname: '.hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'index.hbs', //layout principal
+    layoutsDir: __dirname + '/views/layouts', //carpeta de layout
+    partialsDir: __dirname + '/views/partials',// carpeta de partials
   })
 );
+//Fin de configuracion del motor
 
 let productsHC = [
   { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
@@ -28,7 +33,39 @@ let productsHC = [
   { id: 3, title: 'adidas shoes', price: 102, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
 ];
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
   //sirve productslist.hbs en index.hbs (index.hbs es la plantilla por defecto donde arranca todo)
+  //ACA LA PAGINA ESTA ARMADA  EN BACK
   res.render('productslist', { products: productsHC, productsExist: true });
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  try {
+    let productoEncontrado =  productsHC.find(e => e.id == id);
+    
+    if(productoEncontrado){
+    res.render('product', {producto: productoEncontrado, title: 'Detalle del producto'})
+    } else {
+      res.render('noProduct')
+    }
+  } catch (error) {
+    res.json({error});
+  }
+})
+
+app.get('/form', (req, res) => {
+  res.render('formulario');
+});
+
+app.post('/form', (req, res) => {
+  const { body } = req;
+
+  const indice = productsHC.map(elem => elem.id).sort();
+  id = indice[indice.length - 1] + 1 
+  
+  const productoAgregar = {...body, id}
+
+  productsHC.push(productoAgregar);
+  res.render('productoNuevo', { productoNuevo: productoAgregar, title:'Nuevo producto agregado'});
 });
